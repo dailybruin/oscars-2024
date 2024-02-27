eda966ea50e3fbd1cfa8a45ecedfa457585fa161
Maria Campo Martins <mariacampomartins@gmail.com>
2024-02-26 18:41:43 -0800

    Added Font

---
+++ src/components/RelatedCoverageCard.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    font-family: 'PorterSansBlock', sans-serif;
+`
+
+const RelatedCoverageCard = () => {
+    return (
+        <Container> Hello </Container>
+    )
+  }
+  
+  export default RelatedCoverageCard;
