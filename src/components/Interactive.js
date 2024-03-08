9739881ab159e1e060d3ef06f8e03515cf5dacdf
Nicole Ju <nicoleju@g.ucla.edu>
2024-03-07 22:35:29 -0800

    interactive

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -35,18 +35,23 @@ function InteractiveSection(props) {
 
     let vertical_offset = props.vertical_offset ? props.vertical_offset : 0;
 
+    const twitterCode = `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Cinema&#39;s biggest night is quickly approaching.<br><br>From Best Picture to Best Cinematography, 24 award categories are up for grabs at the 96th annual Academy Awards. Which film do you think will take home the most awards?</p>&mdash; Daily Bruin (@dailybruin) <a href="https://twitter.com/dailybruin/status/1765921810863026299?ref_src=twsrc%5Etfw">March 8, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`;
+
     return (
         <Container style={{ marginTop: vertical_offset }}>
             <Container>
                 <BG src={Interactive} />
                 <TitleText>INTERACTIVE</TitleText>
-            </Container>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
             <iframe
-                src="https://x.com/dailybruin/status/1765921810863026299?s=20"
-            width="80%"
-            height="100%"
-            style={{ border: "none", position: "relative", zIndex: 5}}  // Correct way to apply styles in JSX
-        ></iframe>
+                srcDoc={twitterCode}
+                width="100%"
+                height="100%"
+                style={{ border: "none", zIndex: 5, paddingLeft:'30%'}}
+            ></iframe>
+            /</div>
+            </Container>
+            
         </Container>
     )
 }
